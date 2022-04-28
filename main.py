28da3328eb7c4b3ef382f61e58e8a5eb1cbbdebc
ljxk <102322415+ljxk@users.noreply.github.com>
2022-04-28 14:46:00 +0000

    g

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pygame, random
 pygame.init()
-print ("nothin happenin")
+print ("thats a pretty cool color")
 window = pygame.display.set_mode((600,400))
-pygame.display.set_caption("cool game")
+pygame.display.set_caption("coob game")
 yellow = (255,255,000)
 white = (255,255,255)
 greenbutdifferent = (37,217,115)
 bluebutdifferent = (000,136,255)
+randomcolor = (random.randint(0,255),random.randint(0,255),random.randint(0,255))
 speed = 5 
 cubeX = 250 
 cubeY = 250 
@@ -14,3 +15,21 @@ cubeSize = 20
 run = True
 food = [random.randrange(1,500),random.randrange(1,500), 10, 10] 
 Bfood = [random.randrange(1,500),random.randrange(1,500), 10, 10]
+
+while run:
+  window.fill(randomcolor)
+  food_status = True
+  Bfood_status = True
+  keys = pygame.key.get_pressed()
+  if keys[pygame.K_UP]:
+    print ("up")
+  if keys[pygame.K_LEFT]:
+    print ("left")
+  if keys[pygame.K_RIGHT]:
+    print ("right")
+  if keys[pygame.K_DOWN]:
+    print ("down")
+
+  pygame.event.pump()
+  pygame.display.update()
+ 
